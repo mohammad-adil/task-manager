9e343a1348dfdf72adc0826093cf873f29a97e27
mohammad-adil <shahaadil13@gmail.com>
2020-10-07 09:23:10 +0530

    hashing password

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
-
+const bcrypt = require('bcrypt')
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -56,7 +56,12 @@ const userSchema = new mongoose.Schema({
 userSchema.pre('save', async function(next) {
     const user = this
 
+    if (user.isModified('password')) {
 
+
+        user.password = await bcrypt.hash(user.password, 8)
+
+    }
 
     next()
 })
