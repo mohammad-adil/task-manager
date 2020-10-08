ac43f8fe20fb8f5ba117fbf371aeb216a57de2b4
mohammad-adil <shahaadil13@gmail.com>
2020-10-08 14:26:26 +0530

    creating tokens for login

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -34,45 +35,55 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         trim: true,
-        lowercase: true,
         required: true,
         minlength: 6,
-        maxlength: 16,
 
         validate(value) {
-            if (value.includes('password')) {
-
-                throw new Error('Password  cannot contain "Password" ')
-
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
             }
 
 
         }
 
-    }
-
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 
 })
 
 
-userSchema.statics.findByCredentials = async(email, password) => {
 
+
+
+userSchema.methods.generateAuthToken = async function() {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
+}
+
+userSchema.statics.findByCredentials = async(email, password) => {
     const user = await User.findOne({ email })
 
     if (!user) {
-
-        throw new Error('Unabale to Login')
-
+        throw new Error('Unable to login')
     }
 
     const isMatch = await bcrypt.compare(password, user.password)
 
     if (!isMatch) {
-        throw new Error('Unable to Login')
+        throw new Error('Unable to login')
     }
 
     return user
-
 }
 
 
