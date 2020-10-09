d3dd4d658b6e15876debcc7e6e71d96f1e37beb9
mohammad-adil <shahaadil13@gmail.com>
2020-10-09 18:24:17 +0530

    updating miidleware

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ const app = express()
 require("./db/mongoose")
 
 const port = process.env.PORT || 3000
-app.use(express.json())
 
+
+app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require("../models/user")
+const auth = require("../middleware/Auth")
 
 const router = new express.Router()
 
@@ -8,8 +9,11 @@ router.post('/users', async(req, res) => {
     const user = new User(req.body)
 
     try {
+
         await user.save()
-        res.status(201).send()
+        const token = await user.generateAuthToken()
+
+        res.status(201).send({ user, token })
     } catch (e) {
 
         res.status(400).send(e)
@@ -34,14 +38,41 @@ router.post('/users/login', async(req, res) => {
 })
 
 
-router.get('/users', async(req, res) => {
+router.post('/users/logout', auth, async(req, res) => {
 
     try {
-        const users = await User.find({})
-        res.send(users)
+
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+        res.send()
+    } catch (e) {
+        res.status(500).save()
+
+    }
+})
+
+
+router.post('/users/logoutAll', auth, async(req, res) => {
+
+    try {
+
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+
     } catch (e) {
         res.status(500).send()
     }
+
+
+})
+
+
+
+router.get('/users/me', auth, async(req, res) => {
+    res.send(req.user)
 })
 
 
